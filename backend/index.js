189a21e5cbc8fdc6acbf2c9f254d5c5813c0d5d0
Abderrazak Bouasker <abderrazakbouasker@gmail.com>
2025-03-04 15:44:29 +0100

    fixed and added docker compose dev env

--- backend/index.js
+++ backend/index.js
@@ -11,22 +11,22 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
 import formRoutes from "./routes/form.js";
-import feedbackRoutes from "./routes/feedback.js";
+// import feedbackRoutes from "./routes/feedback.js";
 
 import { register } from "./controllers/auth.js";
-import { createFeedback } from "./controllers/feedback.js";
+// import { createFeedback } from "./controllers/feedback.js";
 import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({path: '../.env'});
+dotenv.config({path: '.env'});
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "100mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
@@ -43,26 +43,27 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 //ROUTES WITH FILES
-app.post("/feedback", verifyToken, (req, res, next) => {
-  upload.array("pictures", 5)(req, res, (err) => {
-    if (err instanceof multer.MulterError) {
-      return res.status(400).json({ error: err.message });
-    } else if (err) {
-      return res.status(500).json({ error: "An unknown error occurred during file upload." });
-    }
-    next();
-  });
-}, createFeedback);
+// app.post("/feedback", verifyToken, (req, res, next) => {
+//   upload.array("pictures", 5)(req, res, (err) => {
+//     if (err instanceof multer.MulterError) {
+//       return res.status(400).json({ error: err.message });
+//     } else if (err) {
+//       return res.status(500).json({ error: "An unknown error occurred during file upload." });
+//     }
+//     next();
+//   });
+// }, createFeedback);
 
 //ROUTES
 app.post("/auth/register", register);
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/form", formRoutes);
-app.use("/feedback", feedbackRoutes);
+// app.use("/feedback", feedbackRoutes);
 
 //MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
+console.log(process.env.MONGO_URL);
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
