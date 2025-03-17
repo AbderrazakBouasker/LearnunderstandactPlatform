cffcd5f77d0e61efb80c15493f22a4db52649b40
Abderrazak Bouasker <abderrazakbouasker@gmail.com>
2025-03-17 11:40:24 +0100

    removed password from returned user info

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,6 +5,7 @@ export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
+    user.password = undefined;
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
